9d45c58e5b08c23f0e1640554f4a7385c039ec37
Fritz <fre.healy@gmail.com>
2017-06-13 10:49:04 -0400

    adding products page and products post type

--- inc/post-types.php
+++ inc/post-types.php
@@ -8,18 +8,18 @@ function js_custom_init()
 	// Register the Homepage Slides
   
      $labels = array(
-	'name' => _x('Slides', 'post type general name'),
-    'singular_name' => _x('Slide', 'post type singular name'),
-    'add_new' => _x('Add New', 'Slide'),
-    'add_new_item' => __('Add New Slide'),
-    'edit_item' => __('Edit Slides'),
-    'new_item' => __('New Slide'),
-    'view_item' => __('View Slides'),
-    'search_items' => __('Search Slides'),
-    'not_found' =>  __('No Slides found'),
-    'not_found_in_trash' => __('No Slides found in Trash'), 
+	'name' => _x('Products', 'post type general name'),
+    'singular_name' => _x('Product', 'post type singular name'),
+    'add_new' => _x('Add New', 'Product'),
+    'add_new_item' => __('Add New Product'),
+    'edit_item' => __('Edit Products'),
+    'new_item' => __('New Product'),
+    'view_item' => __('View Products'),
+    'search_items' => __('Search Products'),
+    'not_found' =>  __('No Products found'),
+    'not_found_in_trash' => __('No Products found in Trash'), 
     'parent_item_colon' => '',
-    'menu_name' => 'Slides'
+    'menu_name' => 'Products'
   );
   $args = array(
 	'labels' => $labels,
@@ -36,7 +36,7 @@ function js_custom_init()
     'supports' => array('title','editor','custom-fields','thumbnail'),
 	
   ); 
-  register_post_type('slides',$args); // name used in query
+  register_post_type('product',$args); // name used in query
   
   // Add more between here
   
